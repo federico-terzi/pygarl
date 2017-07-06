9bfa46d72279f4870372ebe68a72446cbd54ab24
Federico Terzi <freddy6896@gmail.com>
2017-07-06 22:48:06 +0200

    First SVMClassifier Shell

--- pygarl/classifiers.py
+++ pygarl/classifiers.py
@@ -1,4 +1,22 @@
+from pygarl.abstracts import AbstractClassifier
 
 
-class SVMClassifier(object):
-    pass
+class SVMClassifier(AbstractClassifier):
+    # TODO: implementation and tests
+
+    def save_model(self, model_path):
+        pass
+
+    def load_sample_data(self, sample):
+        pass
+
+    def predict(self, sample):
+        pass
+
+    def train_model(self):
+        pass
+
+    def load_from_file(self, model_path):
+        pass
+
+
