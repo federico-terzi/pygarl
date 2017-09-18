b348700e2dee12517555b4064cf48ce9672cee12
Federico Terzi <freddy6896@gmail.com>
2017-09-18 15:56:59 +0200

    Fixed pyserial bug

--- pygarl/plugins/plist.py
+++ pygarl/plugins/plist.py
@@ -13,4 +13,9 @@ def list_serial_ports():
 
     # Print all the info
     for i, port in enumerate(ports):
-        print(i, ")", port.device, port.description)
+        # Workaround based on the current pySerial version
+        # https://stackoverflow.com/questions/43341926/calling-function-method-throws-attritubeerror-tuple-object-has-no-attribute
+        if hasattr(port, "device"):
+            print(i, ")", port.device, port.description)
+        else:
+            print(i, ")", port[0], port[1])
